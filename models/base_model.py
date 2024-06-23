b2ad5c217119076a2b530db88a63083f4bf7c91c
Moaz Sayed <moaz469654@gmail.com>
2024-06-23 17:14:45 +0200

    moaz

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+import uuid
+import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}){self.__dict__}"
+    
+    
+    
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    
+    
+    
+    def to_dict(self):
+        my_dic = self.__dict__.copy()
+        my_dic['created_at'] = self.created_at.isoformat()
+        my_dic['updated_at'] = self.created_at.isoformat()
+        my_dic['__class__'] = self.__class__.__name__
+        
+        return my_dic
+    
