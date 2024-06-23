527d0e4f483eb68280b0ada96e9ee24e1a495a3b
Moaz Sayed <moaz469654@gmail.com>
2024-06-23 20:34:21 +0200

    first commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,21 @@
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        
+        if kwargs:
+            for key , value in kwargs.items():
+                if key == __class__:
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                self.key = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     
     def __str__(self):
