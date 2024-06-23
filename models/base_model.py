5bfedac3c125f3516803b05312d55376dfebc09b
Moaz Sayed <moaz469654@gmail.com>
2024-06-23 22:29:48 +0200

    update on the base_mpdel

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,18 @@ class BaseModel:
         
         if kwargs:
             for key , value in kwargs.items():
-                if key == __class__:
+                if key == '__class__':
                     continue
+                
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
-                self.key = value
+                    
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                    
+                else:
+                    setattr(self, key, value)
         else:
+            
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -32,7 +38,7 @@ class BaseModel:
     def to_dict(self):
         my_dic = self.__dict__.copy()
         my_dic['created_at'] = self.created_at.isoformat()
-        my_dic['updated_at'] = self.created_at.isoformat()
+        my_dic['updated_at'] = self.updated_at.isoformat()
         my_dic['__class__'] = self.__class__.__name__
         
         return my_dic
